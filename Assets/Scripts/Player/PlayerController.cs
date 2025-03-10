a8bb1c4f600b96dd3d4ed79aa15eb31330b2e29d
Zain Gill <zaingill45@gmail.com>
2025-03-09 20:08:03 -0600

    Refactor player control and remove obsolete scripts
    - Updated Transform components in Prototype.unity to adjust local positions and parent references.
    - Modified MonoBehaviour components to add new properties like defaultSpeed and jumpForce while removing obsolete ones.
    - Removed Player.cs, PlayerCamera.cs, and PlayerCharacter.cs files along with their metadata.
    - Introduced a new PlayerController.cs file implementing ICharacterController for improved player input and movement handling.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,135 @@
+using UnityEngine;
+using Utilities;
+using KCC;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour, ICharacterController
+    {
+        [field: Header("Dependencies")]
+        [field: SerializeField] private KinematicCharacterMotor motor;
+        [field: SerializeField] private Camera cam;
+
+        [field: Header("Camera Settings")]
+        [field: SerializeField, Range(50f, 150f)] private float defaultFOV = 103f;
+        [field: SerializeField, Range(0.01f, 1f)] private float sensitivity = 0.2f;
+        [field: SerializeField, Range(0f, -90f)] private float minVerticalClamp = -90f;
+        [field: SerializeField, Range(0f, 90f)] private float maxVerticalClamp = 90f;
+
+        [field: Header("General Settings")]
+        [field: SerializeField] private float defaultSpeed = 8f;
+        [field: SerializeField] private float jumpForce = 10f;
+        [field: SerializeField] private float gravity = -30f;
+
+        private Vector3 inputVector;
+        private Vector3 smoothedVector;
+        private Vector3 planerImpulseVector;
+
+        private Vector3 controllerRotation;
+        private Vector3 movementDirection;
+
+        private float angleX;
+        private float angleY;
+        private float desiredFOV;
+        private float currentSpeed;
+        private float desiredSpeed;
+        private float defaultVerticalFOV;
+
+        private bool requestedJump;
+
+        private void Start()
+        {
+            defaultVerticalFOV = Camera.HorizontalToVerticalFieldOfView(defaultFOV, ZUtils.DEFAULT_ASPECT_RATIO);
+
+            cam.fieldOfView = defaultVerticalFOV;
+            motor.CharacterController = this;
+            desiredFOV = defaultVerticalFOV;
+            currentSpeed = defaultSpeed;
+            desiredSpeed = defaultSpeed;
+        }
+
+        private void Update()
+        {
+            #region Evaluate Controller Rotation
+            angleX += Input.GetAxisRaw("Mouse X") * sensitivity;
+            angleY -= Input.GetAxisRaw("Mouse Y") * sensitivity;
+
+            angleY = Mathf.Clamp(angleY, minVerticalClamp, maxVerticalClamp);
+
+            cam.transform.eulerAngles = new Vector3(angleY, angleX, 0);
+            controllerRotation = new Vector3(0, angleX, 0);
+
+            cam.transform.position = motor.transform.position + motor.CharacterUp * (motor.Capsule.height - 0.26f);
+            #endregion
+
+            inputVector = (motor.CharacterForward * Input.GetAxisRaw("Vertical") + motor.CharacterRight * Input.GetAxisRaw("Horizontal")).normalized;
+
+            #region Evaluate Jump
+            requestedJump = requestedJump || Input.GetKeyDown(KeyCode.Space) && motor.GroundingStatus.IsStableOnGround;
+            #endregion
+
+            #region Smooth Movement
+            if (motor.GroundingStatus.IsStableOnGround)
+            {
+                if (inputVector.magnitude > 0f)
+                {
+                    smoothedVector = Vector3.Lerp(smoothedVector, inputVector, Time.deltaTime * 9f);
+                }
+                else
+                {
+                    smoothedVector = Vector3.Lerp(smoothedVector, inputVector, Time.deltaTime * 8f);
+                }
+            }
+            else
+            {
+                if (inputVector.magnitude > 0f)
+                {
+                    smoothedVector = Vector3.Lerp(smoothedVector, inputVector, Time.deltaTime * 5f);
+                }
+                else
+                {
+                    smoothedVector = Vector3.Lerp(smoothedVector, inputVector, Time.deltaTime * 0.1f);
+                }
+            }
+            #endregion
+
+            movementDirection = smoothedVector + planerImpulseVector;
+        }
+
+        public void UpdateRotation(ref Quaternion currentRotation, float deltaTime) 
+        {
+            currentRotation = Quaternion.Euler(controllerRotation);
+        }
+        public void UpdateVelocity(ref Vector3 currentVelocity, float deltaTime) 
+        {
+            if (motor.GroundingStatus.IsStableOnGround)
+            {
+                currentVelocity = movementDirection * defaultSpeed;
+            } else
+            {
+                currentVelocity += motor.CharacterUp * gravity * deltaTime;
+            }
+
+            if (requestedJump)
+            {
+                requestedJump = false;
+
+                motor.ForceUnground();
+
+                float currentVerticalSpeed = Vector3.Dot(currentVelocity, motor.CharacterUp);
+                float targetVerticalSpeed = Mathf.Max(currentVerticalSpeed, jumpForce);
+
+                currentVelocity += motor.CharacterUp * (targetVerticalSpeed - currentVerticalSpeed);
+            }
+        }
+        public void AfterCharacterUpdate(float deltaTime) { }
+        public void BeforeCharacterUpdate(float deltaTime) { }
+        public bool IsColliderValidForCollisions(Collider coll) { return true; }
+        public void OnDiscreteCollisionDetected(Collider hitCollider) { }
+        public void OnGroundHit(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint, ref HitStabilityReport hitStabilityReport) { }
+        public void OnMovementHit(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint, ref HitStabilityReport hitStabilityReport) { }
+        public void PostGroundingUpdate(float deltaTime) { }
+        public void ProcessHitStabilityReport(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint, Vector3 atCharacterPosition, Quaternion atCharacterRotation, ref HitStabilityReport hitStabilityReport) { }
+        
+    }
+}
